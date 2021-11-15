d8329ccd0b20b13cefbd04fae413ca00fdfe0089
mohammmadasadm <md.asad@koelapps.com>
2021-11-15 17:12:56 +0530

    rails g controller static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # Almost every application defines a route for the root path ("/") at the top of this file.
-  # root "articles#index"
+  root "static_pages#landing_page"
+  # get 'static_pages/landing_page'
+  # get 'static_pages/privacy_policy'
+  get "privacy_policy", to: "static_pages#privacy_policy"
 end
